dfd56991668e2114a982172c41beac375f465ea3
dhiraj <sharmadheeraj849@gmail.com>
2021-12-29 14:02:53 +0530

    weather api

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
-alert('bolo shyam pyare ki jai')
+// alert('bolo shyam pyare ki jai')
 const cityName = document.getElementById('cityName');
 const submitBtn = document.getElementById('submitBtn');
+const temp = document.getElementById('temp')
 const city_name = document.getElementById('city_name');
+const temp_status = document.getElementById('temp_status')
 
 
 
@@ -14,9 +16,14 @@ const getInfo = async (event)=>{
 
     }else{
         try {
-            url = `api.openweathermap.org/data/2.5/weather?q${cityVal}&units=metric&appid = 6676466360d84b316711ba650a827bec`
-            const responce = await fetch(url)
-            console.log(responce)
+            url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=d17d03aaa209fa0ec27331cdb34d16d8`
+            const responce = await fetch(url);
+            const data = await responce.json();
+            const arrData = [data];
+
+            console.log(arrData);
+            temp.innerText = arrData[0].main.temp;
+            
         } catch (error) {
             city_name.innerText = `pls write the name of the city properly `
         }
