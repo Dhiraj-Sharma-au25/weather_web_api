cf9e76c1022948f4b72d2409ca69edf0f39b98d8
dhiraj <sharmadheeraj849@gmail.com>
2021-12-31 12:42:52 +0530

    weather Api

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,12 @@ const cityName = document.getElementById('cityName');
 const submitBtn = document.getElementById('submitBtn');
 const temp = document.getElementById('temp')
 const city_name = document.getElementById('city_name');
-const temp_status = document.getElementById('temp_status')
+const temp_status = document.getElementById('tempStatus');
+const country_name = document.getElementById('country_name');
+const day = document.getElementById('day')
+
+const cur_city = document.getElementById('cur_city');
+
 
 
 
@@ -20,12 +25,16 @@ const getInfo = async (event)=>{
             const responce = await fetch(url);
             const data = await responce.json();
             const arrData = [data];
-
+            cur_city.innerText = cityVal
             console.log(arrData);
+            
             temp.innerText = arrData[0].main.temp;
+            country_name.innerText = arrData[0].sys.country;
+            
+
             
         } catch (error) {
-            city_name.innerText = `pls write the name of the city properly `
+            city_name.innerText = `Somthing Wrong!`
         }
     }
 }
